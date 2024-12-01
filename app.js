5e79baf06ac6e613b248f47032b92fc82e8ac4a7
ardagnr <arda.fusjn@gmail.com>
2024-12-01 15:21:34 +0300

    Projeyi Mongodb'ye bağladım.Artık istediğim bir veriyi veritabanına ekleyebiliyorum ve görüntüleyebiliyorum.Anasayfama bu veriler dinamik ve sıralı olarak geliyor.

--- app.js
+++ app.js
@@ -1,17 +1,66 @@
 const express = require('express');
 const morgan=require('morgan');
+const mongoose=require('mongoose');
+const Blog=require('./models/blogs');
 const app = express();
-app.set('view engine','ejs');
+const dbURL='mongodb+srv://arda:13211100Arda.@cluster0.g93pw.mongodb.net/?retryWrites=true&w=majority&appName=node-blog';
 
-app.listen(3000);
+mongoose.connect(dbURL)
+.then((result)=>app.listen(3000))
+.catch((err)=>console.log(err));
+
+
+app.set('view engine','ejs');
 
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
+//EKLEME, HEPSİNİ GÖRÜNTÜLEME,ID'YE GÖRE GÖRÜNTÜLEME
+/*
+app.get('/add',(req,res)=>{
+    const blog=new Blog({
+        title:'yeni yazı 2',
+        short:'kısa aciklama',
+        long:'uzun aciklama'
+    });
+    blog.save()
+        .then((result)=>{
+            res.send(result);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+})
+app.get('/all',(req,res)=>{
+    Blog.find()
+        .then((result)=>{
+            res.send(result);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+})
+
+app.get('/single',(req,res)=>{
+    Blog.findById('674c4e204cf1bc9df87a674c')
+        .then((result)=>{
+            res.send(result);
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+})
+*/
 
 app.get('/',(req,res)=>{
 
-    res.render('index',{title:'Anasayfa'});
+    Blog.find().sort({createdAt:-1})
+        .then((result)=>{
+          res.render('index',{title:'Anasayfa',blogs:result});
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
 })
 
 app.get('/about',(req,res)=>{
