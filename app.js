d66116644d8505b42b87ef9c9273003a058a7b1d
ardagnr <arda.fusjn@gmail.com>
2024-12-01 16:27:34 +0300

    Bir admin paneli eklendi ve get,post,delete işlemleri aktif hale getirildi.

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ mongoose.connect(dbURL)
 app.set('view engine','ejs');
 
 app.use(express.static('public'));
+app.use(express.urlencoded({extended:true}));
 app.use(morgan('dev'));
 
 //EKLEME, HEPSİNİ GÖRÜNTÜLEME,ID'YE GÖRE GÖRÜNTÜLEME
@@ -57,6 +58,53 @@ app.get('/',(req,res)=>{
     Blog.find().sort({createdAt:-1})
         .then((result)=>{
           res.render('index',{title:'Anasayfa',blogs:result});
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+})
+
+app.get('/blog/:id',(req,res)=>{
+    const id= req.params.id;
+    Blog.findById(id)
+        .then((result)=>{
+            res.render('blog',{blog:result,title:'Detay'})
+        })
+        .catch((err)=>{
+            res.status(404).render('404',{title:'Sayfa Bulunamadı'});
+        })
+})
+
+app.get('/admin',(req,res)=>{
+    Blog.find().sort({createdAt:-1})
+    .then((result)=>{
+      res.render('admin',{title:'Admin',blogs:result});
+    })
+    .catch((err)=>{
+        console.log(err);
+    })
+})
+
+app.get('/admin/add',(req,res)=>{
+    res.render('add',{title:"Yeni yazi"})
+})
+
+app.post('/admin/add',(req,res)=>{
+    const blog = new Blog(req.body);
+    blog.save()
+        .then((result)=>{
+            res.redirect('/admin');
+        })
+        .catch((err)=>{
+            console.log(err);
+        })
+})
+
+app.delete('/admin/delete/:id',(req,res)=>{
+    const id=req.params.id;
+    Blog.findByIdAndDelete(id)
+        .then((result)=>{
+            res.json({link:'/admin'})
         })
         .catch((err)=>{
             console.log(err);
